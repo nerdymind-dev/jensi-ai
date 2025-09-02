cfeb37388dd30bf22eb7c4478c98771a7f417051
Shaun Parkison <shparkison@gmail.com>
2025-09-02 16:11:40 -0600

    chore: Fix example shortcode comment

--- includes/FrontendLoader.php
+++ includes/FrontendLoader.php
@@ -30,7 +30,7 @@ class FrontendLoader
         $this->prefix = $prefix;
 
         // let say the prefix above is wp_awesome_plugin, then the shortcode becomes
-        // [wp-awesome-plugin-vue-app postfix='frontend']
+        // [jensi_ai-vue-app postfix='frontend']
         $this->id = str_replace('_', '-', $this->prefix.'-vue-app');
         add_shortcode($this->id, [$this, 'render_frontend']);
     }
