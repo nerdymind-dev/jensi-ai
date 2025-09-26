b730b34425d232151e8fbf83d11ce5673e0bd0bf
Shaun Parkison <shparkison@gmail.com>
2025-09-26 08:47:14 -0600

    chore: Cleanup settings page
    Remove agent and DS references since they now live in widgets

--- src/admin/views/AdminSettings.vue
+++ src/admin/views/AdminSettings.vue
@@ -202,136 +202,6 @@
           </div>
         </div>
       </div>
-      
-      <modal 
-        :show="showAgentModal"
-        type="info"
-        :custom-icon="true"
-        @close="showAgentModal = false">
-        <template #icon>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
-            <path fill-rule="evenodd" d="M8.25 6.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM15.75 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM2.25 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM6.31 15.117A6.745 6.745 0 0 1 12 12a6.745 6.745 0 0 1 6.709 7.498.75.75 0 0 1-.372.568A12.696 12.696 0 0 1 12 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 0 1-.372-.568 6.787 6.787 0 0 1 1.019-4.38Z" clip-rule="evenodd" />
-            <path d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z" />
-          </svg>
-        </template>
-        <template #title>
-          JENSi AI Agents
-        </template>
-        <template #content>
-          <div class="w-full">
-            <div class="flex flex-col gap-3">
-              <div class="md:grid grid-cols-5">
-                <label
-                  class="col-span-2 block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4">
-                  Select an agent
-                  <sup class="text-red-400">*</sup>
-                </label>
-                <t-rich-select
-                  class="col-span-3"
-                  v-model="settings.jensi_ai_agent"
-                  :fetch-options="fetchAgentOptions"
-                  valueAttribute="id"
-                  textAttribute="name"
-                  :options="currentAgents"
-                />
-              </div>
-            </div>
-          </div>
-        </template>
-        <template #footer>
-          <t-button variant="secondary" @click.native="showAgentModal = false">
-            Nevermind
-          </t-button>
-          <t-button
-            class="ml-2" @click.native="(e) => doSaveAgent()"
-            :class="{ 'opacity-25 cursor-not-allowed': !agentCantSubmit || isSubmitting }"
-            :disabled="!agentCantSubmit || isSubmitting">
-            Save
-          </t-button>
-        </template>
-      </modal>
-
-      <modal 
-        :show="showDSModal"
-        type="info"
-        :custom-icon="true"
-        @close="showDSModal = false">
-        <template #icon>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
-            <path d="M5.566 4.657A4.505 4.505 0 0 1 6.75 4.5h10.5c.41 0 .806.055 1.183.157A3 3 0 0 0 15.75 3h-7.5a3 3 0 0 0-2.684 1.657ZM2.25 12a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3v-6ZM5.25 7.5c-.41 0-.806.055-1.184.157A3 3 0 0 1 6.75 6h10.5a3 3 0 0 1 2.683 1.657A4.505 4.505 0 0 0 18.75 7.5H5.25Z" />
-          </svg>
-        </template>
-        <template #title>
-          JENSi AI Data Source
-        </template>
-        <template #content>
-          <div class="w-full">
-            <div class="flex flex-col gap-3">
-              <div class="md:grid grid-cols-5">
-                <label
-                  class="col-span-2 block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4">
-                  {{ dataSourceConfig.create_config ? 'Create new data source' : 'Select existing data source' }}
-                </label>
-                <div>
-                  <t-toggle v-model="dataSourceConfig.create_config" />
-                </div>
-              </div>
-
-              <template v-if="dataSourceConfig.create_config">
-                <div class="md:grid grid-cols-5">
-                  <label
-                    class="col-span-2 block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4">
-                    Name
-                    <sup class="text-red-400">*</sup>
-                  </label>
-                  <t-input
-                    class="col-span-3"
-                    v-model="dataSourceConfig.name"
-                  />
-                </div>
-                <div class="md:grid grid-cols-5">
-                  <label
-                    class="col-span-2 block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4">
-                    Description
-                  </label>
-                  <t-textarea
-                    class="col-span-3"
-                    v-model="dataSourceConfig.description"
-                  />
-                </div>
-              </template>
-              <template v-else>
-                  <div class="md:grid grid-cols-5">
-                  <label
-                    class="col-span-2 block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4">
-                    Current data sources
-                    <sup class="text-red-400">*</sup>
-                  </label>
-                  <t-rich-select
-                    class="col-span-3"
-                    v-model="dataSourceConfig.config_id"
-                    :fetch-options="fetchDsOptions"
-                    valueAttribute="id"
-                    textAttribute="name"
-                    :options="currentDataSources"
-                  />
-                </div>
-              </template>
-            </div>
-          </div>
-        </template>
-        <template #footer>
-          <t-button variant="secondary" @click.native="showDSModal = false">
-            Nevermind
-          </t-button>
-          <t-button
-            class="ml-2" @click.native="(e) => doSaveDataSource()"
-            :class="{ 'opacity-25 cursor-not-allowed': !dsCanSubmit || isSubmitting }"
-            :disabled="!dsCanSubmit || isSubmitting">
-            Save
-          </t-button>
-        </template>
-      </modal>
     </section>
   </div>
 </template>
@@ -340,8 +210,7 @@
 import {onBeforeMount, reactive, computed, ref, nextTick, toRaw, inject} from 'vue'
 import {TToggle, TButton, TRichSelect, TTextarea, TInput, TSelect} from '@variantjs/vue'
 import Slider from '@vueform/slider'
-import Breadcrumbs from '~src/shared/components/BreadcrumbNavigation.vue'
-import Modal from '~src/shared/components/ConfirmationModal.vue'
+// import Breadcrumbs from '~src/shared/components/BreadcrumbNavigation.vue'
 import ColorInput from 'vue-color-input'
 
 import {VAceEditor} from 'vue3-ace-editor'
@@ -354,14 +223,6 @@ ace.config.set(
 const win = inject('win')
 const swal = inject('swal')
 const axios = inject('axios')
-
-const defaultDsConfig = {
-  create_config: true,
-  name: '',
-  description: '',
-  config_id: null,
-  type: 'wordpress',
-}
 
 const config = inject('pluginConfig')
 
@@ -384,7 +245,6 @@ const currentAgents = ref([])
 const showDSModal = ref(false)
 const selectedDataSource = ref(null)
 const currentDataSources = ref([])
-const dataSourceConfig = ref({ ...defaultDsConfig })
 
 const hasChanged = computed(() => {
   // compare two objects
@@ -393,35 +253,6 @@ const hasChanged = computed(() => {
   // eslint-disable-next-line vue/no-side-effects-in-computed-properties
   ui.actionKey = ui.actionKey + 1
   return (a === b)
-})
-
-const agentCantSubmit = computed(() => {
-  if (isSubmitting.value) {
-    return false
-  }
-  if (!settings.jensi_ai_agent) {
-    return false
-  }
-  return true
-})
-
-const dsCanSubmit = computed(() => {
-  if (isSubmitting.value) {
-    return false
-  }
-  if (dataSourceConfig.value.create_config) {
-    if (!dataSourceConfig.value.name) {
-      return false
-    }
-  } else {
-    if (!dataSourceConfig.value.config_id) {
-      return false
-    }
-    if (dataSourceConfig.value.config_id === settings.jensi_ai_data_source) {
-      return false
-    }
-  }
-  return true
 })
 
 onBeforeMount(() => {
@@ -454,152 +285,6 @@ const resetToDefaults = () => {
     showConfirmButton: false,
     timer: 1500
   })
-}
-
-const fetchAgentOptions = async (q) => {
-  try {
-    // Need to add the bearer token to the request
-    const rst = await axios.get(endpoints.value.agents.get, {
-      params: {
-        search: q
-      },
-    })
-    if (rst.status === 200) {
-      return {
-        results: rst.data.data || []
-      }
-    }
-  } catch (err) {
-    if (err?.code !== 'ERR_CANCELED') {
-      const text = err?.response?.data?.error || 'Server responded with an error'
-      swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text,
-        footer: '<div class="overflow-footer w-full">' + err.message + '</div>'
-      })
-    }
-  }
-  return []
-}
-
-const fetchDsOptions = async (q) => {
-  try {
-    // Need to add the bearer token to the request
-    const rst = await axios.get(endpoints.value.data_sources.get, {
-      params: {
-        agent_id: settings.jensi_ai_agent,
-        search: q
-      },
-    })
-    if (rst.status === 200) {
-      return {
-        results: rst.data.data || []
-      }
-    }
-  } catch (err) {
-    if (err?.code !== 'ERR_CANCELED') {
-      const text = err?.response?.data?.error || 'Server responded with an error'
-      swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text,
-        footer: '<div class="overflow-footer w-full">' + err.message + '</div>'
-      })
-    }
-  }
-  return []
-}
-
-const doSaveAgent = async () => {
-  if (!agentCantSubmit.value) {
-    return
-  }
-
-  // Fetch the data sources for the selected agent
-  const options = await fetchDsOptions('')
-  console.log('options', options)
-  currentDataSources.value = options.results || []
-
-  // If the current selected data source is not in the list, clear it
-  if (settings.jensi_ai_data_source) {
-    const existing = currentDataSources.value.find((item) => item.id === settings.jensi_ai_data_source)
-    if (existing) {
-      selectedDataSource.value = existing
-    } else {
-      selectedDataSource.value = null
-      settings.jensi_ai_data_source = null
-    }
-  }
-
-  // Just save the settings
-  await doSave()
-
-  // Close the modal
-  showAgentModal.value = false
-}
-
-const doSaveDataSource = async () => {
-  if (!dsCanSubmit.value) {
-    return
-  }
-
-  isSubmitting.value = true
-  try {
-    if (dataSourceConfig.value.create_config) {
-      const rst = await axios.post(endpoints.value.data_sources.create, Object.assign(dataSourceConfig.value, {
-        agent_id: settings.jensi_ai_agent,
-      }))
-      if (rst.status === 200) {
-        const config = rst.data.data || null
-        if (config) {
-          // Set the selected data source
-          currentDataSources.value.push(config)
-          selectedDataSource.value = config
-          settings.jensi_ai_data_source = config.id
-          
-          // Save the settings and close the modal
-          await doSave()
-          showDSModal.value = false
-
-          // Reset the data source config
-          dataSourceConfig.value = { ...defaultDsConfig }
-        }
-      } else {
-        this.$swal.fire({
-          icon: 'error',
-          title: 'Error',
-          text: 'API responded with an error',
-          footer: '<div class="overflow-footer w-full">' + JSON.stringify(rst, null, 2) + '</div>'
-        })
-      }
-    } else {
-      // Just save the selected config id to the settings
-      settings.jensi_ai_data_source = dataSourceConfig.value.config_id
-      selectedDataSource.value = currentDataSources.value.find((item) => item.id === dataSourceConfig.value.config_id)
-
-      // Save the settings and close the modal
-      await doSave()
-      showDSModal.value = false
-
-      // Reset the data source config
-      dataSourceConfig.value = { ...defaultDsConfig }
-    }
-  } catch (err) {
-    if (err?.code !== 'ERR_CANCELED') {
-      console.log(err)
-      const text = err?.response?.data?.message || 'Server responded with an error'
-      swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text,
-        footer: '<div class="overflow-footer w-full">' + err.message + '</div>'
-      })
-    }
-  }
-  finally {
-    isSubmitting.value = false
-  }
 }
 
 const doSave = async () => {
